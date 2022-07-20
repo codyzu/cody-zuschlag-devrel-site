0abf54b26db6c96ff236bcc6bcf0f5f3afe17b12
codyzu <cody.zuschlag@gmail.com>
2022-07-20 16:13:31 +0200

    enable analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import viteRadar from 'vite-plugin-radar';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteRadar({analytics: [{id: 'G-DMTB5F3X2D'}]})],
 });
