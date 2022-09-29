0dd074a9ab48f5ff55a21b69b96d84c2cf8720c7
Cody Zuschlag <cody.zuschlag@gmail.com>
2022-09-29 09:49:02 +0200

    add articles

--- src/articles.ts
+++ src/articles.ts
@@ -2,9 +2,60 @@ import {Article} from './article-type';
 
 const articles: Article[] = [
   {
+    title: 'Building a Design System of Systems',
+    url: 'https://www.nearform.com/blog/building-a-design-system-of-systems/',
+    date: '2022-05-17T12:00Z',
+  },
+  {
+    title: 'Unraveling the CORS mystery',
+    url: 'https://www.nearform.com/blog/the-cors-mystery/',
+    date: '2021-01-05T12:00Z',
+  },
+  {
+    title: 'The JSONata Performance Dilemma',
+    url: 'https://www.nearform.com/blog/the-jsonata-performance-dilemma/',
+    date: '2020-11-19T12:00Z',
+  },
+  {
+    title: 'Solving the serverless concurrency problem with Google Cloud Run',
+    url: 'https://www.nearform.com/blog/solving-the-serverless-concurrency-problem-with-google-cloud-run/',
+    date: '2020-10-06T12:00Z',
+  },
+  {
     title: 'Cloud Firestore Geo Queries',
     url: 'https://medium.com/@codyzus/cloud-firestore-geo-queries-a0e095b781c6',
     date: '2020-05-12T12:00Z',
+  },
+  {
+    title: 'Lock down with Cloud Firestore',
+    url: 'https://medium.com/@codyzus/lock-down-with-cloud-firestore-61a98d1f4647',
+    date: '2020-05-06T12:00Z',
+  },
+  {
+    title: 'Continuous Deployment for Serverless Google Cloud Function',
+    url: 'https://blog.hubwa.re/continuous-deployment-for-serverless-google-cloud-functions-175b0756be28',
+    date: '2018-03-27T12:00Z',
+  },
+  {
+    title: 'Google Cloud Platform cron jobs sourced from Kubernetes',
+    url: 'https://blog.hubwa.re/google-cloud-platform-cron-jobs-sourced-from-kubernetes-33a4f6046e3f',
+    date: '2018-03-06T12:00Z',
+  },
+  {
+    title:
+      'Google Cloud PubSub background job queue with Cloud Function workers',
+    url: 'https://blog.hubwa.re/google-cloud-pubsub-background-job-queue-with-cloud-function-workers-78092ce5a8cb',
+    date: '2018-02-22T12:00Z',
+  },
+  {
+    title: "Don't nock our integration tests",
+    url: 'https://blog.hubwa.re/dont-nock-our-integration-tests-fa9682ba7155',
+    date: '2017-12-14T12:00Z',
+  },
+  {
+    title: 'NodeConf EU: An incredible conference for an incredible platform',
+    url: 'https://blog.hubwa.re/nodeconf-eu-an-incredible-conference-for-an-incredible-platform-c04fcc9be856',
+    date: '2017-11-30T12:00Z',
   },
 ];
 
