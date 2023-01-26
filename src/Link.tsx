0cd41b43cf32eb7fc565f4d7bb1264f3cc953396
Cody Zuschlag <cody.zuschlag@gmail.com>
2023-01-26 16:24:56 +0100

    track link navigation

--- src/Link.tsx
+++ src/Link.tsx
@@ -1,4 +1,5 @@
-import {type ReactNode} from 'react';
+import {type MouseEvent, type ReactNode} from 'react';
+import {track, parameters} from 'insights-js';
 
 export function Link({
   icon,
@@ -9,8 +10,25 @@ export function Link({
   url: string;
   children: ReactNode;
 }) {
+  function nav() {
+    if (import.meta.env.PROD) {
+      console.log('click');
+      track({
+        id: 'nav',
+        parameters: {
+          url,
+          screenType: parameters.screenType(),
+          referrer: parameters.referrer(),
+          locale: parameters.locale(),
+        },
+      });
+      console.log('go');
+    }
+  }
+
   return (
     <a
+      onClick={nav}
       href={url}
       className="link decoration-none inline-flex flex-row items-center gap-2"
     >
