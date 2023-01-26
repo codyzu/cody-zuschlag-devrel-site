e1fe4dda1e4b31f2380e319d9584dd6121d29f50
Cody Zuschlag <cody.zuschlag@gmail.com>
2023-01-26 12:17:16 +0100

    improve talk grid layout

--- src/Talk.tsx
+++ src/Talk.tsx
@@ -9,47 +9,49 @@ type Props = {
 export default function Talk({talk}: Props) {
   return (
     <>
-      <div className="text-2xl -mb-3">{talk.conference}</div>
-      <div className="ml-7 grid grid-cols-1 sm:grid-cols-[2fr_1fr] md:grid-cols-[3fr_1fr_1fr_1fr] text-secondary gap-y-2 gap-x-4">
-        <div>{talk.name}</div>
-        <div>{new Date(talk.date).toLocaleDateString()}</div>
-        <div>
-          <div className="flex flex-row items-center gap-2">
-            <div
-              className={cx(
-                talk.flag ?? 'i-lucide-globe text-white',
-                'min-w-[1rem] min-h-[1rem]',
-              )}
-            />
-            <div>{talk.location}</div>
+      <div className="text-2xl sm:col-span-2 md:col-span-4 mt-4 first:mt-0">
+        {talk.conference}
+      </div>
+      <div className="ml-7 text-secondary">{talk.name}</div>
+      <div className="lt-sm:ml-7 text-secondary">
+        {new Date(talk.date).toLocaleDateString()}
+      </div>
+      <div className="lt-md:ml-7 text-secondary">
+        <div className="flex flex-row items-center gap-2">
+          <div
+            className={cx(
+              talk.flag ?? 'i-lucide-globe text-primary',
+              'min-w-[1rem] min-h-[1rem]',
+            )}
+          />
+          <div>{talk.location}</div>
+        </div>
+      </div>
+      <div className="flex flex-col items-start lt-sm:ml-7 text-secondary">
+        {talk.video === 'none' ? (
+          <div className="inline-flex flex-row items-center gap-2">
+            <div className="i-lucide-video-off" />
+            Not recorded
           </div>
-        </div>
-        <div className="flex flex-col items-start">
-          {talk.video === 'none' ? (
-            <div className="inline-flex flex-row items-center gap-2">
-              <div className="i-lucide-video-off" />
-              Not recorded
-            </div>
-          ) : talk.video ? (
-            <Link icon="i-lucide-video" url={talk.video}>
-              Watch now
-            </Link>
-          ) : (
-            <div className="bg-blue-600 text-black rounded-full px-2">
-              Video coming soon
-            </div>
-          )}
-          {talk.slides && (
-            <Link icon="i-lucide-monitor" url={talk.slides}>
-              Slides
-            </Link>
-          )}
-          {talk.repo && (
-            <Link icon="i-lucide-github" url={talk.repo}>
-              GitHub repo
-            </Link>
-          )}
-        </div>
+        ) : talk.video ? (
+          <Link icon="i-lucide-video" url={talk.video}>
+            Watch now
+          </Link>
+        ) : (
+          <div className="bg-highlight text-black rounded-full px-2 font-600 text-sm">
+            Coming soon
+          </div>
+        )}
+        {talk.slides && (
+          <Link icon="i-lucide-monitor" url={talk.slides}>
+            Slides
+          </Link>
+        )}
+        {talk.repo && (
+          <Link icon="i-lucide-github" url={talk.repo}>
+            GitHub repo
+          </Link>
+        )}
       </div>
     </>
   );
--- src/TalkList.tsx
+++ src/TalkList.tsx
@@ -10,9 +10,11 @@ export default function Talks() {
         title="Speaking engagements and videos"
         subtitle="All of my previous and planned future speaking experience"
       />
-      {talks.map((talk) => (
-        <Talk key={`${talk.name}-${talk.date}`} talk={talk} />
-      ))}
+      <div className="grid grid-cols-1 sm:grid-cols-[1fr_auto] md:grid-cols-[3fr_auto_1fr_auto] lg:grid-cols-[1fr_auto_auto_auto] gap-y-1 gap-x-4">
+        {talks.map((talk) => (
+          <Talk key={`${talk.name}-${talk.date}`} talk={talk} />
+        ))}
+      </div>
     </Section>
   );
 }
