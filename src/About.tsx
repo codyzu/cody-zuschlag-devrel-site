3c8dcd19c22a780453e48e28030092687c1586e6
Cody Zuschlag <cody.zuschlag@gmail.com>
2023-02-15 13:50:30 -0800

    make nearform logo a link

--- src/About.tsx
+++ src/About.tsx
@@ -8,14 +8,17 @@ export default function About() {
     <Section>
       <div className="grid grid-cols-1 lg:grid-cols-2">
         <div className="mb-12 lg:mb-0 px-0 lg:px-3">
-          <div className="aspect-video w-full min-h-0 flex flex-col items-center max-h-[400px]">
+          <a
+            href="https://nearform.com"
+            className="aspect-video w-full min-h-0 flex flex-col items-center max-h-[400px]"
+          >
             <img
               src={logo}
               className="block lg:mx-auto rounded max-w-full h-auto object-cover h-full"
-              alt="Bootstrap Themes"
+              alt="NearForm logo"
               loading="lazy"
             />
-          </div>
+          </a>
         </div>
         <div className="self-start lg:-order-1 flex flex-col gap-4">
           <SectionTitle title="About me" />
