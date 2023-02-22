9457c64fe6de4f16a2c78d4d1662b8958a1ddc0a
Cody Zuschlag <cody.zuschlag@gmail.com>
2023-02-22 12:23:32 -0800

    layout with react table

--- src/TalkList.tsx
+++ src/TalkList.tsx
@@ -1,9 +1,101 @@
+import {
+  createColumnHelper,
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+} from '@tanstack/react-table';
+import cx from 'clsx';
 import Talk from './Talk';
 import talks from './talks';
 import Section from './Section';
 import SectionTitle from './SectionTitle';
+import {Link} from './Link';
+import type TalkType from './talk-type';
+
+const columnHelper = createColumnHelper<TalkType>();
+const defaultColumns = [
+  columnHelper.accessor('conference', {
+    cell: (cell) => (
+      <div className="text-2xl sm:col-span-2 md:col-span-4 mt-4 first:mt-0 text-primary">
+        {cell.getValue()}
+      </div>
+    ),
+  }),
+  columnHelper.accessor('name', {
+    cell: (cell) => (
+      <div className="ml-7 text-secondary">{cell.getValue()}</div>
+    ),
+  }),
+  columnHelper.accessor('date', {
+    cell: (cell) => (
+      <div className="lt-sm:ml-7 text-secondary">
+        {new Date(cell.getValue()).toLocaleDateString()}
+      </div>
+    ),
+  }),
+  columnHelper.accessor('location', {
+    cell: (cell) => (
+      <div className="lt-md:ml-7 text-secondary">
+        <div className="flex flex-row items-center gap-2">
+          <div
+            className={cx(
+              cell.row.original.flag ?? 'i-lucide-globe text-primary',
+              'min-w-[1rem] min-h-[1rem]',
+            )}
+          />
+          <div>{cell.getValue()}</div>
+        </div>
+      </div>
+    ),
+  }),
+  columnHelper.accessor('video', {
+    cell: (cell) => (
+      <div className="sm:col-start-2 md:col-start-4 lt-sm:ml-7">
+        {cell.getValue() === 'none' ? (
+          <Link icon="i-lucide-video-off text-lg" active={false}>
+            Not recorded
+          </Link>
+        ) : cell.getValue() ? (
+          <Link icon="i-lucide-video text-lg" url={cell.getValue()}>
+            <div className="">Watch now</div>
+          </Link>
+        ) : (
+          <Link icon="i-lucide-timer text-lg" active={false}>
+            Coming soon
+          </Link>
+        )}
+      </div>
+    ),
+  }),
+  columnHelper.accessor('slides', {
+    cell: (cell) =>
+      cell.getValue() && (
+        <div className="sm:col-start-2 md:col-start-4 lt-sm:ml-7">
+          <Link icon="i-lucide-monitor text-lg" url={cell.getValue()}>
+            Slides
+          </Link>
+        </div>
+      ),
+  }),
+  columnHelper.accessor('repo', {
+    cell: (cell) =>
+      cell.getValue() && (
+        <div className="sm:col-start-2 md:col-start-4 lt-sm:ml-7">
+          <Link icon="i-lucide-github text-lg" url={cell.getValue()}>
+            GitHub repo
+          </Link>
+        </div>
+      ),
+  }),
+];
 
 export default function Talks() {
+  const table = useReactTable({
+    data: talks,
+    columns: defaultColumns,
+    getCoreRowModel: getCoreRowModel(),
+    debugAll: true,
+  });
   return (
     <Section id="talks">
       <SectionTitle
@@ -14,6 +106,17 @@ export default function Talks() {
         {talks.map((talk) => (
           <Talk key={`${talk.name}-${talk.date}`} talk={talk} />
         ))}
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-[1fr_auto] md:grid-cols-[3fr_auto_1fr_auto] lg:grid-cols-[1fr_auto_auto_auto] gap-y-1 gap-x-4 text-secondary">
+        {table
+          .getRowModel()
+          .rows.map((row) =>
+            row
+              .getVisibleCells()
+              .map((cell) =>
+                flexRender(cell.column.columnDef.cell, cell.getContext()),
+              ),
+          )}
       </div>
     </Section>
   );
